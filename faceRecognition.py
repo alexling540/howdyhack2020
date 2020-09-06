8f34429ce616abed67bf4e8e56a6c39607de0205
Bryce Bodner <bodnerbryce@gmail.com>
2020-09-05 23:08:57 -0500

    Updated Facial Recog

--- faceRecognition.py
+++ faceRecognition.py
@@ -10,7 +10,7 @@ glasses = cv2.imread("images/glasses2.png", -1)
 mustache = cv2.imread('images/mustache.png', -1)
 
 
-def glasses_stache(imagePath):
+def glasses_stache(imagePath, rectangles):
     image = cv2.imread(imagePath)
 
     img = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
@@ -20,37 +20,44 @@ def glasses_stache(imagePath):
     for (x, y, w, h) in faces:
         roi_gray = gray[y:y + h, x:x + h]  # rec
         roi_color = img[y:y + h, x:x + h]
-        # cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 3)
+        if rectangles:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
         eyes = eyes_cascade.detectMultiScale(roi_gray, scaleFactor=1.5, minNeighbors=5)
         for (ex, ey, ew, eh) in eyes:
-            # cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
             roi_eyes = roi_gray[ey: ey + eh, ex: ex + ew]
-            glasses2 = image_resize(glasses.copy(), width=ew)
 
-            gw, gh, gc = glasses2.shape
-            for i in range(0, gw):
-                for j in range(0, gh):
-                    # print(glasses[i, j]) #RGBA
-                    if glasses2[i, j][3] != 0:  # alpha 0
-                        roi_color[ey + i, ex + j] = glasses2[i, j]
+            if rectangles:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 3)
+
+            else:
+                glasses2 = image_resize(glasses.copy(), width=ew)
+                gw, gh, gc = glasses2.shape
+                for i in range(0, gw):
+                    for j in range(0, gh):
+                        # print(glasses[i, j]) #RGBA
+                        if glasses2[i, j][3] != 0:  # alpha 0
+                            roi_color[ey + i, ex + j] = glasses2[i, j]
 
         nose = nose_cascade.detectMultiScale(roi_gray, scaleFactor=1.5, minNeighbors=5)
         for (nx, ny, nw, nh) in nose:
-            # cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (255, 0, 0), 3)
             roi_nose = roi_gray[ny: ny + nh, nx: nx + nw]
-            mustache2 = image_resize(mustache.copy(), width=nw)
 
-            mw, mh, mc = mustache2.shape
-            for i in range(0, mw):
-                for j in range(0, mh):
-                    # print(glasses[i, j]) #RGBA
-                    if mustache2[i, j][3] != 0:  # alpha 0
-                        roi_color[ny + int(nh / 2.0) + i, nx + j] = mustache2[i, j]
+            if rectangles:
+                cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (0, 0, 255), 3)
+
+            else:
+                mustache2 = image_resize(mustache.copy(), width=nw)
+                mw, mh, mc = mustache2.shape
+                for i in range(0, mw):
+                    for j in range(0, mh):
+                        # print(glasses[i, j]) #RGBA
+                        if mustache2[i, j][3] != 0:  # alpha 0
+                            roi_color[ny + int(nh / 2.0) + i, nx + j] = mustache2[i, j]
 
     final_img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
     cv2.imwrite(imagePath, final_img)
 
 
 if __name__ == "__main__":
-    glasses_stache('images/face8.png')
+    glasses_stache('images/face1-Copy.jpg', True)
