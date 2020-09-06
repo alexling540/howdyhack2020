45b37077808f51793e8e6bf4a90cf0709062d866
Daniel <danielchamorro.q@gmail.com>
2020-09-05 22:37:15 -0500

    moved files to new location

--- src/components/pages/disguise.js
+++ src/components/pages/disguise.js
@@ -8,6 +8,15 @@ export default function face_rec() {
     
     <>
     <h1 className='disguise'>DISGUISE</h1>;
+
+    
+    <form method = 'post' enctype='multipart/form-data'>
+      <p>
+        <input type='file' name='file'/>
+        <input type='submit' value='Upload'/>
+      </p>
+    </form>
+
       <Footer />
     </>
   );
