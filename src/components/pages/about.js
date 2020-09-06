0c63994c18b3714d49322e45ab329385b5f3a2a2
Daniel <danielchamorro.q@gmail.com>
2020-09-05 23:34:33 -0500

    added about paragraphs

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import Paragraphs from '../Paragraphs'
 
 export default function About() {
   return (
     <>
     <h1 className='about'>ABOUT</h1>;
+      <Paragraphs/>
       <Footer />
     </>
   );
--- src/components/pages/face_rec.js
+++ src/components/pages/face_rec.js
@@ -7,6 +7,15 @@ export default function face_rec() {
   return (
     <>
     <h1 className='face_rec'>FACIAL RECOGNITION</h1>;
+
+    <form method = 'post' enctype='multipart/form-data'>
+      <p>
+        <input type='file' name='file'/>
+        <input type='submit' value='Upload'/>
+      </p>
+    </form>
+
+
       <Footer />
     </>
   );
