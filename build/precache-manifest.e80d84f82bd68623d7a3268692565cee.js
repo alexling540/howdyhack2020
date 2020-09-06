637df35a2cd1fe2366cf835f4a9d91cbf9424074
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-09-06 01:50:15 -0500

    Stop ignoring build folder

---
+++ build/precache-manifest.e80d84f82bd68623d7a3268692565cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bb7b03d1480d720b17ad2e8f5651d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ca6505d0e06fdf6b1b",
+    "url": "/static/css/main.339c0458.chunk.css"
+  },
+  {
+    "revision": "6d4dd4da736ca90954e9",
+    "url": "/static/js/2.1193bc77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1193bc77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ca6505d0e06fdf6b1b",
+    "url": "/static/js/main.4d56826d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
