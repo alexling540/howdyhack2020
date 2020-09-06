960a45b711c96d3e19bd32bab20f7e41be1980ce
Daniel <danielchamorro.q@gmail.com>
2020-09-06 01:35:50 -0500

    changed to radio input

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import '../../App.css';
+
+import './Cards.css';
+import CardItem from './CardItem';
+
 import Footer from '../Footer';
 import Paragraphs from '../Paragraphs'
+
 
 export default function About() {
   return (
     <>
     <h1 className='about'>ABOUT</h1>;
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-9.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-9.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+
+
+
       <Paragraphs/>
       <Footer />
     </>
