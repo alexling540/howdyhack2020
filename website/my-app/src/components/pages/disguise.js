581903a1f9e2728e9ce77797a1b83003eb74abcc
Daniel <danielchamorro.q@gmail.com>
2020-09-05 22:24:46 -0500

    website

---
+++ website/my-app/src/components/pages/disguise.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function face_rec() {
+
+  return (
+    
+    <>
+    <h1 className='disguise'>DISGUISE</h1>;
+      <Footer />
+    </>
+  );
+}
---
+++ website/my-app/src/components/pages/face_rec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function face_rec() {
+
+  return (
+    <>
+    <h1 className='face_rec'>FACIAL RECOGNITION</h1>;
+      <Footer />
+    </>
+  );
+}
